549749e1cf75df05837143a14870fcfbb54a3781
naatii <natiworlds@gmail.com>
2023-10-10 19:39:27 +0200

    Script para construir proyectos de programación

--- autoBuild.sh
+++ autoBuild.sh
@@ -1,56 +1,56 @@
-@echo off
+#!/bin/bash
 
-set/p ruta= Indica el path absoluto local: 
+echo -n "Indica el path absoluto local: "
+read ruta
 
-:pregunta
-@ECHO.1.- GitHub 
-@ECHO.2.- Windows
-@ECHO.3.- ayuda 
-@ECHO.4.- salir
-:pregunta
+pregunta() {
+  echo "1.- GitHub"
+  echo "2.- Windows"
+  echo "3.- ayuda"
+  echo "4.- salir"
+}
 
-set/p pregunta= ¿Que quieres hacer? 
-if %pregunta% == 1 goto GitHub 
-if %pregunta% == 2 goto Build
-if %pregunta% == 3 goto ayuda 
-if %pregunta% == 4 goto salir
+while true; do
+  pregunta
+  echo -n "¿Que quieres hacer? "
+  read pregunta
 
-goto pregunta
-:GitHub
-cd %ruta%
-set/p repo= indique el path del repo para clonarlo: 
-git clone %repo%
-cd %repo%
-echo %repo%
-echo.
-cls
-
-goto pregunta
-:Build
-@echo.Se va a crear la estructura del proyecto para Windows
-set/p ruta= Indica el path absoluto local: 
-cd %ruta%
-mkdir src
-mkdir test
-python -m virtualenv venv
-cd ./venv/bin/
-call activate
-pip install pytest
-Echo ./venv/bin/activate | Clip.exe
-cls
-
-goto pregunta
-echo.
-:ayuda
-echo Esta es la ayuda del programa AutoGit por Natalia Cortés: 
-echo    -1 GitHub (baja el repositorio de GitHub)
-echo    -2 Build (Construye el proyecto)
-echo    -3 ayuda (Muestra este menú)        
-echo    -4 salir (Sale del programa)
-
-goto pregunta
-echo.
-:salir
-echo Pulsa una tecla para salir.
-pause>nul
-exit
+  case $pregunta in
+    1)
+      echo -n "Indique el path del repo para clonarlo: "
+      read repo
+      cd "$ruta" || exit
+      git clone "$repo"
+      cd "$repo" || exit
+      echo "$repo"
+      echo
+      ;;
+    2)
+      echo "Se va a crear la estructura del proyecto para Windows"
+      echo -n "Indica el path absoluto local: "
+      read ruta
+      cd "$ruta" || exit
+      mkdir src
+      mkdir test
+      python3 -m venv venv
+      source venv/bin/activate
+      pip install pytest
+      echo "source venv/bin/activate" | xclip -selection clipboard
+      ;;
+    3)
+      echo "Esta es la ayuda del programa AutoGit por Natalia Cortés:"
+      echo "    -1 GitHub (baja el repositorio de GitHub)"
+      echo "    -2 Build (Construye el proyecto)"
+      echo "    -3 ayuda (Muestra este menú)"
+      echo "    -4 salir (Sale del programa)"
+      ;;
+    4)
+      echo "Pulsa una tecla para salir."
+      read -n 1 -s
+      exit
+      ;;
+    *)
+      echo "Opción no válida, por favor selecciona una opción válida."
+      ;;
+  esac
+done

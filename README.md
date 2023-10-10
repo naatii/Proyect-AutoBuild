a5ad0727c90ab7bdea400160aead4e4a5b993515
naatii <natiworlds@gmail.com>
2023-10-10 19:30:25 +0200

    Script para construir proyectos de programación

---
+++ README.md
@@ -0,0 +1,21 @@
+# AutoBuild proyects
+
+###### por ➡️ Natalia Cortés
+
+![foto de python](./assets/python.png)
+
+Este pequeño script automatiza la creación de proyectos para los alumnos de 1º de DAW 
+
+## Uso del programa
+
+El programa pedirá primeramente una ruta para trabajar con el proyecto donde el usuario quiera clonarse primeramente el repositorio aportado por la moodle
+
+En segundo lugar se mostará el menú al usuario y el 2º paso sería construir el proyecto con la 2ª opción `Build`.
+
+## Opciones 
+    1.- GitHub ( Esto sirve para clonar el repositorio de la tarea en una carpeta que el usuario indique ).
+    2.- Build ( sirve para construir el proyecto como tal ).
+    3.- ayuda ( muestra esta ayuda ).
+    4.- salir ( sale del programa ).
+
+## 
---
+++ autoBuild.bat
@@ -0,0 +1,57 @@
+@echo off
+
+set/p ruta= Indica el path absoluto local: 
+
+:pregunta
+@ECHO.1.- GitHub 
+@ECHO.2.- Windows
+@ECHO.3.- ayuda 
+@ECHO.4.- salir
+:pregunta
+
+set/p pregunta= ¿Que quieres hacer? 
+if %pregunta% == 1 goto GitHub 
+if %pregunta% == 2 goto Build
+if %pregunta% == 3 goto ayuda 
+if %pregunta% == 4 goto salir
+
+goto pregunta
+:GitHub
+cd %ruta%
+set/p repo= indique el path del repo para clonarlo: 
+git clone %repo%
+cd %repo%
+echo %repo%
+echo.
+cls
+
+goto pregunta
+:Build
+@echo.Se va a crear la estructura del proyecto para Windows
+set/p ruta= Indica el path absoluto local: 
+cd %ruta%
+mkdir src
+mkdir test
+python -m virtualenv venv
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+cd .\venv\Scripts\
+call activate.bat 
+pip install pytest
+Echo .\venv\Scripts\activate | Clip.exe
+cls
+
+goto pregunta
+echo.
+:ayuda
+echo Esta es la ayuda del programa AutoGit por Natalia Cortés: 
+echo    -1 GitHub (baja el repositorio de GitHub)
+echo    -2 Build (Construye el proyecto)
+echo    -3 ayuda (Muestra este menú)        
+echo    -4 salir (Sale del programa)
+
+goto pregunta
+echo.
+:salir
+echo Pulsa una tecla para salir.
+pause>nul
+exit
